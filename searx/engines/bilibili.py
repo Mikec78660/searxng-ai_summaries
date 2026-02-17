searxng/searxng

[refactor] duration strings: move parsing logic to utils.py

searx/engines/bilibili.py
@@ -56,18 +56,6 @@ def request(query, params):
     return params
 
 
-# Format the video duration
-def format_duration(duration):
-    if not ":" in duration:
-        return None
-    minutes, seconds = map(int, duration.split(":"))
-    total_seconds = minutes * 60 + seconds
-
-    formatted_duration = str(timedelta(seconds=total_seconds))[2:] if 0 <= total_seconds < 3600 else ""
-
-    return formatted_duration
-
-
 def response(resp):
     search_res = resp.json()
 
@@ -83,7 +71,12 @@ def response(resp):
         unix_date = item["pubdate"]
 
         formatted_date = datetime.fromtimestamp(unix_date)
-        formatted_duration = format_duration(item["duration"])
+
+        # the duration only seems to be valid if the video is less than 60 mins
+        duration = utils.parse_timediff_string(item["duration"])
+        if duration and duration > timedelta(minutes=60):
+            duration = None
+
         iframe_url = f"https://player.bilibili.com/player.html?aid={video_id}&high_quality=1&autoplay=false&danmaku=0"
 
         results.append(
@@ -93,7 +86,7 @@ def response(resp):
                 "content": description,
                 "author": author,
                 "publishedDate": formatted_date,
-                "length": formatted_duration,
+                "length": duration,
                 "thumbnail": thumbnail,
                 "iframe_src": iframe_url,
                 "template": "videos.html",

searx/engines/iqiyi.py
@@ -2,9 +2,10 @@
 """iQiyi: A search engine for retrieving videos from iQiyi."""
 
 from urllib.parse import urlencode
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from searx.exceptions import SearxEngineAPIException
+from searx.utils import parse_timediff_string
 
 about = {
     "website": "https://www.iqiyi.com/",
@@ -55,20 +56,7 @@ def response(resp):
             except (ValueError, TypeError):
                 pass
 
-        length = None
-        subscript_content = album_info.get("subscriptContent")
-        if subscript_content:
-            try:
-                time_parts = subscript_content.split(":")
-                if len(time_parts) == 2:
-                    minutes, seconds = map(int, time_parts)
-                    length = timedelta(minutes=minutes, seconds=seconds)
-                elif len(time_parts) == 3:
-                    hours, minutes, seconds = map(int, time_parts)
-                    length = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-            except (ValueError, TypeError):
-                pass
-
+        length = parse_timediff_string(album_info.get("subscriptionContent"))
         results.append(
             {
                 'url': album_info.get("pageUrl", "").replace("http://", "https://"),

searx/engines/presearch.py
@@ -73,7 +73,7 @@ Implementations
 from urllib.parse import urlencode, urlparse
 from searx import locales
 from searx.network import get
-from searx.utils import gen_useragent, html_to_text
+from searx.utils import gen_useragent, html_to_text, parse_timediff_string
 
 about = {
     "website": "https://presearch.io",
@@ -270,7 +270,7 @@ def response(resp):
                     'url': item.get('link'),
                     'content': item.get('description', ''),
                     'thumbnail': item.get('image'),
-                    'length': item.get('duration'),
+                    'length': parse_timediff_string(item.get('duration')),
                 }
             )
 

Mikec78660/searxng-ai_summaries

searx/engines/bilibili.py
@@ -56,18 +56,6 @@ def request(query, params):
     return params
 
 
-# Format the video duration
-def format_duration(duration):
-    if not ":" in duration:
-        return None
-    minutes, seconds = map(int, duration.split(":"))
-    total_seconds = minutes * 60 + seconds
-
-    formatted_duration = str(timedelta(seconds=total_seconds))[2:] if 0 <= total_seconds < 3600 else ""
-
-    return formatted_duration
-
-
 def response(resp):
     search_res = resp.json()
 
@@ -83,7 +71,12 @@ def response(resp):
         unix_date = item["pubdate"]
 
         formatted_date = datetime.fromtimestamp(unix_date)
-        formatted_duration = format_duration(item["duration"])
+
+        # the duration only seems to be valid if the video is less than 60 mins
+        duration = utils.parse_duration_string(item["duration"])
+        if duration and duration > timedelta(minutes=60):
+            duration = None
+
         iframe_url = f"https://player.bilibili.com/player.html?aid={video_id}&high_quality=1&autoplay=false&danmaku=0"
 
         results.append(
@@ -93,7 +86,7 @@ def response(resp):
                 "content": description,
                 "author": author,
                 "publishedDate": formatted_date,
-                "length": formatted_duration,
+                "length": duration,
                 "thumbnail": thumbnail,
                 "iframe_src": iframe_url,
                 "template": "videos.html",

searx/engines/iqiyi.py
@@ -2,9 +2,10 @@
 """iQiyi: A search engine for retrieving videos from iQiyi."""
 
 from urllib.parse import urlencode
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from searx.exceptions import SearxEngineAPIException
+from searx.utils import parse_duration_string
 
 about = {
     "website": "https://www.iqiyi.com/",
@@ -55,20 +56,7 @@ def response(resp):
             except (ValueError, TypeError):
                 pass
 
-        length = None
-        subscript_content = album_info.get("subscriptContent")
-        if subscript_content:
-            try:
-                time_parts = subscript_content.split(":")
-                if len(time_parts) == 2:
-                    minutes, seconds = map(int, time_parts)
-                    length = timedelta(minutes=minutes, seconds=seconds)
-                elif len(time_parts) == 3:
-                    hours, minutes, seconds = map(int, time_parts)
-                    length = timedelta(hours=hours, minutes=minutes, seconds=seconds)
-            except (ValueError, TypeError):
-                pass
-
+        length = parse_duration_string(album_info.get("subscriptionContent"))
         results.append(
             {
                 'url': album_info.get("pageUrl", "").replace("http://", "https://"),

searx/engines/presearch.py
@@ -73,7 +73,7 @@ Implementations
 from urllib.parse import urlencode, urlparse
 from searx import locales
 from searx.network import get
-from searx.utils import gen_useragent, html_to_text
+from searx.utils import gen_useragent, html_to_text, parse_duration_string
 
 about = {
     "website": "https://presearch.io",
@@ -270,7 +270,7 @@ def response(resp):
                     'url': item.get('link'),
                     'content': item.get('description', ''),
                     'thumbnail': item.get('image'),
-                    'length': item.get('duration'),
+                    'length': parse_duration_string(item.get('duration')),
                 }
             )
 